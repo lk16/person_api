8d58a9821c31dc49cd27c3e2e95a5c0f02250e6e
Luuk Verweij <luuk_verweij@msn.com>
2021-09-10 17:00:25 +0200

    add all endpoints and README examples

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 This is a really small demo for echo in golang on a Friday afternoon
+
+
+### How to use:
+
+```sh
+curl -X POST http://localhost:1323/persons -H 'Content-Type: application/json' -d '{"name": "Luuk", "age": 30}'
+curl http://localhost:1323/persons
+curl http://localhost:1323/persons/81
+curl -X DELETE http://localhost:1323/persons/81
+curl http://localhost:1323/persons
+```
--- main.go
+++ main.go
@@ -1,19 +1,93 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
 
-func helloHandler(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, Rigon!")
+type Person struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+	ID   int    `json:"id"`
+}
+
+var personDB map[int]Person
+
+func listPersons(c echo.Context) error {
+	personsList := []Person{}
+
+	for _, person := range personDB {
+		personsList = append(personsList, person)
+	}
+
+	return c.JSON(http.StatusOK, personsList)
+}
+
+func createPerson(c echo.Context) error {
+	var person Person
+
+	if err := c.Bind(&person); err != nil {
+		return err
+	}
+
+	id := rand.Intn(100)
+	person.ID = id
+
+	personDB[id] = person
+	return c.JSON(http.StatusOK, person)
+}
+
+func getPerson(c echo.Context) error {
+	idString := c.Param("id")
+
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, "id parameter is not an integer")
+	}
+
+	person, ok := personDB[id]
+	if !ok {
+		return c.JSON(http.StatusNotFound, "Could not find person")
+	}
+
+	return c.JSON(http.StatusOK, person)
+}
+
+func deletePerson(c echo.Context) error {
+	idString := c.Param("id")
+
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, "id parameter is not an integer")
+	}
+
+	_, ok := personDB[id]
+	if !ok {
+		return c.JSON(http.StatusNotFound, "Could not find person")
+	}
+
+	delete(personDB, id)
+	return c.NoContent(http.StatusOK)
+}
+
+func greetingHandler(c echo.Context) error {
+	user := c.Param("user")
+	response := fmt.Sprintf("Hello, %s\n", user)
+	return c.String(http.StatusOK, response)
 }
 
 func main() {
+	personDB = make(map[int]Person)
 	e := echo.New()
-
-	e.GET("/", helloHandler)
+	e.GET("/greet/:user", greetingHandler)
+	e.GET("/persons", listPersons)
+	e.POST("/persons", createPerson)
+	e.GET("/persons/:id", getPerson)
+	e.DELETE("/persons/:id", deletePerson)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
